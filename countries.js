606976311b09cb5432d8d3d4e17582690a398d4b
Sherif Elmetwally <sge5@njit.edu>
2020-07-11 15:13:03 -0400

    break countries into 9 regions

---
+++ countries.js
@@ -0,0 +1,43 @@
+let africa = [
+    'Angola', 'Benin', 'Botswana', 'Burkina Faso', 'Burundi', 'Cabo Verde', 'Cameroon', 'Central African Republic', 'Chad',
+    'Comoros', 'Congo, Rep', "Cote d'Ivoire", 'Equatorial Guinea', 'Eritrea', 'Swaziland', 'Ethiopia', 'Gabon', 'Gambia',
+    'Ghana', 'Guinea', 'Guinea-Bissau', 'Kenya', 'Lesotho', 'Liberia', 'Madagascar', 'Malawi', 'Mali', 'Mauritania', 
+    'Mauritius', 'Mozambique', 'Namibia', 'Niger', 'Nigeria', 'Rwanda', 'Sao Tome and Principe', 'Senegal', 'Seychelles',
+    'Sierra Leone', 'Somalia', 'South Africa', 'South Sudan', 'Sudan', 'Tanzania', 'Togo', 'Uganda', 'Zambia', 'Zimbabwe'
+]
+
+let mena = [
+    'Algeria', 'Bahrain', 'Egypt', 'Iran', 'Iraq', 'Israel', 'Jordan', 'Kuwait', 'Lebanon', 'Libya', 'Morocco', 'Oman',
+    'Palestine', 'Qatar', 'Saudi Arabia', 'Syria', 'Tunisia', 'Turkey', 'United Srab Emirates', 'Yemen', 'Djibouti'
+]
+
+let eastAsia = ['China', 'Japan', 'Korea', 'Korea, North', 'Hong Kong SAR', 'Taiwan (Chinese Taipei)', 'Macao']
+
+let india = ['India', 'Pakistan', 'Bangladesh', 'Sri Lanka', 'Bhutan', 'Maldives', 'Nepal']
+
+let centralAsia = 
+[
+    'Kazakhstan', 'Kyrgyzstan', 'Tajikistan', 'Turkmenistan', 'Uzbekistan', 'Armenia', 'Azerbaijan','Georgia', 'Mongolia'
+]
+
+let southEastAsia = 
+[
+    'Brunei', 'Cambodia', 'East Timor', 'Singapore', 'Thailand', 'Vietnam', 'Philippines', 'Malaysia', 'Laos', 'Myanmar', 
+    'Indonesia',
+]
+
+let oceania = [
+    'Australia', 'New Zealand', 'Fiji', 'Samoa', 'Micronesia', 'Vanuatu', 'Guam', 'Kiribati', 'Tonga', 'Palau', 'Papua New Guinea',
+    'Tuvalu', 'Marshall Islands', 'New Caledonia', 'French Polynesia', ''
+]
+
+let westernEurope = [
+    'France', 'Germany', 'Spain', 'Portugal', 'Belgium', 'Austria', 'Andorra', 
+    'Denmark', 'Finland', 'Iceland', 'Norway', 'Sweden', 'United Kingdom', 'Italy', 'Switzerland', 'Netherlands', 'Luxembourg',
+    'Monaco', 'San Marino', 'Isle of Man', 'Faeroe Islands', 'Ireland'
+]
+
+let easternEurope = [
+    'Russia', 'Czech Republic', 'Poland', 'Croatia', 'Slovakia', 'Hungary', 'Romania', 'Moldova', 'Serbia', 'Lithuania',
+    'Latvia', 'Estonia','Slovenia', 'Bulgaria', 'Ukraine', 'Belarus', 'Montenegro', 'Bosnia & Herzegovina', 'Albania', 'Kosovo', 'Macedonia'
+]
--- main.js
+++ main.js
@@ -1,27 +1,70 @@
+/* |||||||||||||||||| REGIONS |||||||||||||||||||| */
+let africa = [
+    'Angola', 'Benin', 'Botswana', 'Burkina Faso', 'Burundi', 'Cabo Verde', 'Cameroon', 'Central African Republic', 'Chad',
+    'Comoros', 'Congo, Rep.', "Cote d'Ivoire", 'Equatorial Guinea', 'Eritrea', 'Swaziland', 'Ethiopia', 'Gabon', 'Gambia',
+    'Ghana', 'Guinea', 'Guinea-Bissau', 'Kenya', 'Lesotho', 'Liberia', 'Madagascar', 'Malawi', 'Mali', 'Mauritania', 
+    'Mauritius', 'Mozambique', 'Namibia', 'Niger', 'Nigeria', 'Rwanda', 'Sao Tome and Principe', 'Senegal', 'Seychelles',
+    'Sierra Leone', 'Somalia', 'South Africa', 'South Sudan', 'Sudan', 'Tanzania', 'Togo', 'Uganda', 'Zambia', 'Zimbabwe', 
+    'Mayotte', 'Congo, Dem. Rep.', 'Reunion',
+]
+
+let mena = [
+    'Algeria', 'Bahrain', 'Egypt', 'Iran', 'Iraq', 'Israel', 'Jordan', 'Kuwait', 'Lebanon', 'Libya', 'Morocco', 'Oman',
+    'Palestine', 'Qatar', 'Saudi Arabia', 'Syria', 'Tunisia', 'Turkey', 'United Srab Emirates', 'Yemen', 'Djibouti',
+    'United Arab Emirates'
+]
+
+let eastAsia = ['China', 'Japan', 'Korea', 'Korea, North', 'Hong Kong SAR', 'Taiwan (Chinese Taipei)', 'Macao']
+
+let india = ['India', 'Pakistan', 'Bangladesh', 'Sri Lanka', 'Bhutan', 'Maldives', 'Nepal']
+
+let centralAsia = 
+[
+    'Kazakhstan', 'Kyrgyzstan', 'Tajikistan', 'Turkmenistan', 'Uzbekistan', 'Armenia', 'Azerbaijan','Georgia', 'Mongolia', 'Afghanistan'
+]
+
+let southEastAsia = 
+[
+    'Brunei', 'Cambodia', 'East Timor', 'Singapore', 'Thailand', 'Vietnam', 'Philippines', 'Malaysia', 'Laos', 'Myanmar', 
+    'Indonesia',
+]
+
+let oceania = [
+    'Australia', 'New Zealand', 'Fiji', 'Samoa', 'Micronesia', 'Vanuatu', 'Guam', 'Kiribati', 'Tonga', 'Palau', 'Papua New Guinea',
+    'Tuvalu', 'Marshall Islands', 'New Caledonia', 'French Polynesia', 'Timor-Leste', 'Solomon Islands', 'American Samoa'
+]
+
+let westernEurope = [
+    'France', 'Germany', 'Spain', 'Portugal', 'Belgium', 'Austria', 'Andorra', 
+    'Denmark', 'Finland', 'Iceland', 'Norway', 'Sweden', 'United Kingdom', 'Italy', 'Switzerland', 'Netherlands', 'Luxembourg',
+    'Monaco', 'San Marino', 'Isle of Man', 'Faeroe Islands', 'Ireland', 'Liechtenstein', 'Greece', 'Cyprus', 'Malta'
+]
+
+let easternEurope = [
+    'Russia', 'Czech Republic', 'Poland', 'Croatia', 'Slovakia', 'Hungary', 'Romania', 'Moldova', 'Serbia', 'Lithuania',
+    'Latvia', 'Estonia','Slovenia', 'Bulgaria', 'Ukraine', 'Belarus', 'Montenegro', 'Bosnia & Herzegovina', 'Albania', 'Kosovo', 'Macedonia'
+]
+
+/* |||||||||||||||||| REGIONS |||||||||||||||||||| */
 // set the dimensions and margins of the graph
-var margin = {top: 10, right: 10, bottom: 10, left: 10},
-  width = 445 - margin.left - margin.right,
-  height = 445 - margin.top - margin.bottom;
-
-// append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
-.append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-.append("g")
-  .attr("transform",
-    "translate(" + margin.left + "," + margin.top + ")");
-
 d3.csv("wealth.csv", function(error, data) {
     const countries = data;
     let realCountries = []
     let continentTotals = []
     let totalWealth = 0;
-    let asiaPacific = []
-    let africa = []
-    let europe = []
-    let northAmerica = []
-    let latinAmerica = []
+    
+    let africanCountries = []
+    let menaCountries = []
+    let easternEuropeanCountries = []
+    let westernEuropeanCountries = []
+    let eastAsianCountries = []
+    let centralAsianCountries = []
+    let southEastAsianCountries = []
+    let indianSubcontinentCountries = []
+    let northAmericanCountries = []
+    let latinAmericanCountries = []
+    let oceaniaCountries = []
+    let leftover = []
     countries.forEach(country => {
         let countryObj = {
             name: country.Country,
@@ -31,110 +74,150 @@ d3.csv("wealth.csv", function(error, data) {
         }
         realCountries.push(countryObj)
     })
+
     realCountries.forEach(country => {
         let countryWealth = country.wealth
-        if (country.region == 'Africa') {
-            africa.push(country)
-        } else if (country.region == 'Asia-Pacific' || country.region == 'China' || country.region == 'India') {
-            asiaPacific.push(country)
+        if (africa.includes(country.name)) {
+            africanCountries.push(country)
+        } else if (mena.includes(country.name)) {
+            menaCountries.push(country)
+        } else if (eastAsia.includes(country.name)) {
+            eastAsianCountries.push(country)
+        } else if (easternEurope.includes(country.name)) {
+            easternEuropeanCountries.push(country)
+        } else if (westernEurope.includes(country.name)) {
+            westernEuropeanCountries.push(country)
+        } else if (india.includes(country.name)) {
+            indianSubcontinentCountries.push(country)
+        } else if (centralAsia.includes(country.name)) {
+            centralAsianCountries.push(country)
+        } else if (southEastAsia.includes(country.name)) {
+            southEastAsianCountries.push(country)
+        } else if (oceania.includes(country.name)) {
+            oceaniaCountries.push(country)
+        } else if (country.region == 'North America') {
+            northAmericanCountries.push(country)
         } else if (country.region == 'Latin America') {
-            latinAmerica.push(country)
-        } else if (country.region == 'Europe') {
-            europe.push(country)
+            latinAmericanCountries.push(country)
         } else {
-            northAmerica.push(country)
+            leftover.push(country)
         }
         totalWealth += countryWealth
     })
     console.log('realCountries: ', realCountries)
 
-    let africaTotal = 0
-    africa.forEach(country => {
-        africaTotal += country.wealth
-    })
-    africa.push({
-        name: 'Africa Total',
-        region: 'Africa',
-        total: africaTotal,
-        percentage: africaTotal / totalWealth * 100
-    })
+  
+    // let africanCountries = []
+    // let menaCountries = []
+    // let easternEuropeanCountries = []
+    // let westernEuropeanCountries = []
+    // let eastAsianCountries = []
+    // let centralAsianCountries = []
+    // let southEastAsianCountries = []
+    // let indianSubcontinentCountries = []
+    // let northAmericanCountries = []
+    // let latinAmericanCountries = []
+    // let oceaniaCountries = []
+    // let leftover = []
 
-    continentTotals.push({
-        name: 'Africa',
-        total: africaTotal,
-        percentage: africaTotal / totalWealth * 100
-    })
+    console.log('africanCountries: ', africanCountries)
+    console.log('menaCountries: ', menaCountries)
+    console.log('easternEuropeanCountries: ', easternEuropeanCountries)
+    console.log('westernEuropeanCountries: ', westernEuropeanCountries)
+    console.log('easternEuropeanCountries: ', easternEuropeanCountries)
+    console.log('eastAsianCountries: ', eastAsianCountries)
+    console.log('southEastAsianCountries: ', southEastAsianCountries)
+    console.log('indianSubcontinentCountries: ', indianSubcontinentCountries)
+    console.log('northAmericanCountries: ', northAmericanCountries)
+    console.log('latinAmericanCountries: ', latinAmericanCountries)
+    console.log('oceaniaCountries: ', oceaniaCountries)
+    console.log('leftover: ', leftover)
 
-    let asiaPacificTotal = 0
-    asiaPacific.forEach(country => {
-        asiaPacificTotal += country.wealth
-    })
-    asiaPacific.push({
-        name: 'Asia-Pacific Total',
-        region: 'Asia-Pacific',
-        total: asiaPacificTotal,
-        percentage: asiaPacificTotal / totalWealth * 100
-    })
-    continentTotals.push({
-        name: 'Asia-Pacific',
-        total: asiaPacificTotal,
-        percentage: asiaPacificTotal / totalWealth * 100
-    })
-
-    let europeTotal = 0
-    europe.forEach(country => {
-        europeTotal += country.wealth
-    })
-    europe.push({
-        name: 'Europe Total',
-        region: 'Europe',
-        total: europeTotal,
-        percentage: europeTotal / totalWealth * 100
-    })
-    continentTotals.push({
-        name: 'Europe',
-        total: europeTotal,
-        percentage: europeTotal / totalWealth * 100
-    })
-
-    let northAmericaTotal = 0
-    northAmerica.forEach(country => {
-        northAmericaTotal += country.wealth
-    })
-    northAmerica.push({
-        name: 'North America Total',
-        region: 'North America',
-        total: northAmericaTotal,
-        percentage: northAmericaTotal / totalWealth * 100
-    })
-    continentTotals.push({
-        name: 'North America',
-        total: northAmericaTotal,
-        percentage: northAmericaTotal / totalWealth * 100
-    })
-
-    let latinAmericaTotal = 0
-    latinAmerica.forEach(country => {
-        latinAmericaTotal += country.wealth
-    })
-    latinAmerica.push({
-        name: 'Latin America Total',
-        region: 'Latin America',
-        total: latinAmericaTotal,
-        percentage: latinAmericaTotal / totalWealth * 100
-    })
-
-    continentTotals.push({
-        name: 'Latin America',
-        total: latinAmericaTotal,
-        percentage: latinAmericaTotal / totalWealth * 100
-    })
-    console.log('africaTotal: ', africaTotal)
-    console.log('totalWealth: ', totalWealth)
-    console.log('asiaPacific: ', asiaPacific)
-    console.log('africa: ', africa)
-    console.log('europe: ', europe)
-    console.log('northAmerica: ', northAmerica)
-    console.log('latinAmerica: ', latinAmerica)
-    console.log('continentTotals: ', continentTotals)
 })
+
+
+
+
+
+
+// let africaTotal = 0
+// africa.forEach(country => {
+//     africaTotal += country.wealth
+// })
+// africa.push({
+//     name: 'Africa Total',
+//     region: 'Africa',
+//     total: africaTotal,
+//     percentage: africaTotal / totalWealth * 100
+// })
+
+// continentTotals.push({
+//     name: 'Africa',
+//     total: africaTotal,
+//     percentage: africaTotal / totalWealth * 100
+// })
+
+// let asiaPacificTotal = 0
+// asiaPacific.forEach(country => {
+//     asiaPacificTotal += country.wealth
+// })
+// asiaPacific.push({
+//     name: 'Asia-Pacific Total',
+//     region: 'Asia-Pacific',
+//     total: asiaPacificTotal,
+//     percentage: asiaPacificTotal / totalWealth * 100
+// })
+// continentTotals.push({
+//     name: 'Asia-Pacific',
+//     total: asiaPacificTotal,
+//     percentage: asiaPacificTotal / totalWealth * 100
+// })
+
+// let europeTotal = 0
+// europe.forEach(country => {
+//     europeTotal += country.wealth
+// })
+// europe.push({
+//     name: 'Europe Total',
+//     region: 'Europe',
+//     total: europeTotal,
+//     percentage: europeTotal / totalWealth * 100
+// })
+// continentTotals.push({
+//     name: 'Europe',
+//     total: europeTotal,
+//     percentage: europeTotal / totalWealth * 100
+// })
+
+// let northAmericaTotal = 0
+// northAmerica.forEach(country => {
+//     northAmericaTotal += country.wealth
+// })
+// northAmerica.push({
+//     name: 'North America Total',
+//     region: 'North America',
+//     total: northAmericaTotal,
+//     percentage: northAmericaTotal / totalWealth * 100
+// })
+// continentTotals.push({
+//     name: 'North America',
+//     total: northAmericaTotal,
+//     percentage: northAmericaTotal / totalWealth * 100
+// })
+
+// let latinAmericaTotal = 0
+// latinAmerica.forEach(country => {
+//     latinAmericaTotal += country.wealth
+// })
+// latinAmerica.push({
+//     name: 'Latin America Total',
+//     region: 'Latin America',
+//     total: latinAmericaTotal,
+//     percentage: latinAmericaTotal / totalWealth * 100
+// })
+
+// continentTotals.push({
+//     name: 'Latin America',
+//     total: latinAmericaTotal,
+//     percentage: latinAmericaTotal / totalWealth * 100
+// })
